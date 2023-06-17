e3748fe7bfc9c8e626a630e234c6e1edfc0349f8
YN1kiforov <yakov-nikiforov.2@mail.ru>
2023-06-17 16:32:50 +0700

    Checkbox  refactoring

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Field, useField } from 'formik';
+import { useField } from 'formik';
 
 import s from './style.module.scss'
 
@@ -9,13 +9,14 @@ interface CheckboxGroupProps {
   label: string;
 }
 
-export const CheckboxGroup: React.FC<CheckboxGroupProps> = ({ name, children, label }) => {
+export const CheckboxGroup = ({ name, children, label }: CheckboxGroupProps) => {
   const [field, meta, helpers] = useField(name);
-  const handleChange = (value: string, checked: boolean) => {
-    if (checked) {
-      helpers.setValue([...meta.value, value]);
+  const handleChange = (value: number) => {
+    const currentValue = meta.value;
+    if (currentValue.includes(value)) {
+      helpers.setValue(currentValue.filter((val: number) => val !== value));
     } else {
-      helpers.setValue(meta.value.filter((val: string) => val !== value));
+      helpers.setValue([...currentValue, value]);
     }
   };
 
@@ -23,167 +24,28 @@ export const CheckboxGroup: React.FC<CheckboxGroupProps> = ({ name, children, la
     <>
       <label className={s.label}>{label}</label>
       {children.map((child) => (
-        React.cloneElement(child, {
-          name,
-          checked: field.value.includes(child.props.value),
-          onChange: (e: any) => handleChange(child.props.value, e.target.checked),
-        })
+        <Checkbox
+          key={child.props.value}
+          name={name}
+          checked={field.value.includes(child.props.value)}
+          onChange={() => handleChange(child.props.value)}
+          {...child.props}
+        >
+          {child.props.children}
+        </Checkbox>
       ))}
     </>
   );
 };
 
 interface CheckboxProps {
-  name?: string;
   value: number;
-  label?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  checked?: boolean;
   children: ReactNode;
 }
 
-export const Checkbox = ({ value, checked, onChange, children, name }: CheckboxProps) => {
-
+export const Checkbox = ({ children, ...props }: CheckboxProps) => {
   return <label>
-    <Field type="checkbox" name={name} value={value} checked={checked}
-      onChange={onChange} />
+    <input type="checkbox" {...props} />
     {children}
   </label>
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import s from './style.module.scss'
-// import React from 'react';
-// import { useField } from 'formik';
-
-// interface CheckboxGroupProps {
-//   name: string;
-//   options: {
-//     value: string;
-//     label: string;
-//   }[];
-// }
-
-// const CheckboxGroup: React.FC<CheckboxGroupProps> = ({ name, options }) => {
-//   const [, meta, helpers] = useField(name);
-
-//   const handleChange = (value: string, checked: boolean) => {
-//     if (checked) {
-//       helpers.setValue([...(meta.value || []), value]);
-//     } else {
-//       helpers.setValue((meta.value || []).filter((val: string) => val !== value));
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {options.map((option) => (
-//         <Checkbox
-//           key={option.value}
-//           name={name}
-//           value={option.value}
-//           label={option.label}
-//           onChange={handleChange}
-//           checked={meta.value && meta.value.includes(option.value)}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// interface CheckboxProps {
-//   name: string;
-//   value: string;
-//   label: string;
-//   onChange: (value: string, checked: boolean) => void;
-//   checked: boolean;
-// }
-
-// const Checkbox: React.FC<CheckboxProps> = ({
-//   name,
-//   value,
-//   label,
-//   onChange,
-//   checked,
-// }) => {
-//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     onChange(value, event.target.checked);
-//   };
-
-//   return (
-//     <div>
-//       <label>
-//         <input
-//           type="checkbox"
-//           name={name}
-//           value={value}
-//           checked={checked}
-//           onChange={handleChange}
-//         />
-//         {label}
-//       </label>
-//     </div>
-//   );
-// };
-
-// export default CheckboxGroup;
