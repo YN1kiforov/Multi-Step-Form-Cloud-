ae7f85fc815bcbb5aed67ecb7259bc2afbe32cf5
YN1kiforov <yakov-nikiforov.2@mail.ru>
2023-07-28 09:59:16 +0700

    test

---
+++ postinstall.js
@@ -0,0 +1,88 @@
+// Добавить загрузки при ожидании
+// Добавить
+
+const readline = require('readline');
+const fs = require('fs');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+const packageJsonPath = './package.json';
+
+function readPackageJson() {
+  const data = fs.readFileSync(packageJsonPath);
+  return JSON.parse(data);
+}
+
+function writePackageJson(data) {
+  fs.writeFileSync(packageJsonPath, JSON.stringify(data, null, 2));
+}
+
+async function updateDependencies(packageJson, outdatedDependencies) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.log('Необновленные зависимости:');
+  outdatedDependencies.forEach((dep) => {
+    console.log(`${dep.name}: ${dep.current} -> ${dep.latest}`);
+  });
+
+  const answer = await new Promise((resolve) => {
+    rl.question('Вы хотите обновить зависимости? (y/n): ', (input) => {
+      resolve(input.toLowerCase());
+      rl.close();
+    });
+  });
+
+  if (answer === 'y') {
+    outdatedDependencies.forEach((dep) => {
+      packageJson.dependencies[dep.name] = `^${dep.latest}`;
+    });
+    writePackageJson(packageJson);
+    console.log('Зависимости успешно обновлены!');
+  } else {
+    console.log('Обновление зависимостей отменено.');
+  }
+}
+
+// Функция для получения последней версии зависимости из npm
+async function getLatestVersionFromNpm(depName) {
+  try {
+    const { stdout, stderr } = await exec(`npm show ${depName} version`);
+    return stdout.trim();
+  } catch (error) {
+    throw new Error(`Не удалось получить последнюю версию для ${depName}: ${error.message}`);
+  }
+}
+
+// Основной код
+async function postinstall() {
+  try {
+    const packageJson = readPackageJson();
+    const outdatedDependencies = [];
+
+    for (const depName in packageJson.dependencies) {
+      const currentVersion = packageJson.dependencies[depName];
+      const latestVersion = await getLatestVersionFromNpm(depName);
+
+      if (currentVersion !== latestVersion) {
+        outdatedDependencies.push({
+          name: depName,
+          current: currentVersion,
+          latest: latestVersion,
+        });
+      }
+    }
+
+    if (outdatedDependencies.length > 0) {
+      await updateDependencies(packageJson, outdatedDependencies);
+    } else {
+      console.log('Все зависимости уже обновлены.');
+    }
+  } catch (error) {
+    console.error('Произошла ошибка:', error.message);
+  }
+}
+
+postinstall();
